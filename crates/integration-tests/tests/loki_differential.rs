5a8e7aca1999f502aefb368f3a8f8ed6d34db906
Matthew Stone <matthew.d.stone@gmail.com>
2026-06-22 10:54:54 -0700

    Match Loki invalid protobuf push errors

--- crates/integration-tests/tests/loki_differential.rs
+++ crates/integration-tests/tests/loki_differential.rs
@@ -5258,6 +5258,31 @@ async fn real_loki_and_crabka_return_same_invalid_snappy_protobuf_push_error() {
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+async fn real_loki_and_crabka_return_same_invalid_protobuf_push_error() {
+    let image = GenericImage::new("grafana/loki", "3.4.2")
+        .with_exposed_port(LOKI_PORT.tcp())
+        .with_wait_for(WaitFor::seconds(2));
+    let loki = image.start().await.expect("start Loki container");
+    let loki_base = format!(
+        "http://127.0.0.1:{}",
+        loki.get_host_port_ipv4(LOKI_PORT)
+            .await
+            .expect("Loki mapped port")
+    );
+    let http = reqwest::Client::new();
+    wait_for_loki_ready(&http, &loki_base).await;
+
+    let distributor = distributor_router_for_status();
+    let payload = vec![0x03, 0x08, 0xff, 0xff, 0xff];
+    let headers = [("content-type", "application/x-protobuf")];
+
+    let loki_result = loki_push_body_result(&http, &loki_base, payload.clone(), &headers).await;
+    let crabka_result = crabka_push_body_result(distributor, payload, &headers).await;
+
+    assert!(crabka_result == loki_result);
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn real_loki_and_crabka_return_same_object_push_timestamp_error() {
     let image = GenericImage::new("grafana/loki", "3.4.2")
         .with_exposed_port(LOKI_PORT.tcp())
