d1d839278485e0296fc50093ad1d937b4970587a
Matthew Stone <matthew.d.stone@gmail.com>
2026-06-22 04:40:21 -0700

    Format sorted Loki label_replace expressions

--- crates/integration-tests/tests/loki_differential.rs
+++ crates/integration-tests/tests/loki_differential.rs
@@ -4303,6 +4303,18 @@ async fn real_loki_and_crabka_return_same_format_query_result() {
 
     assert!(crabka_result == loki_result);
 
+    let query = r#"sort(label_replace(vector(1), "service", "api-$1", "missing", "(.*)"))"#;
+    let loki_result = loki_format_query_result(&http, &loki_base, query).await;
+    let crabka_result = crabka_format_query_result(querier.clone(), query).await;
+
+    assert!(crabka_result == loki_result);
+
+    let query = r#"sort_desc(label_replace(count_over_time({app="api"}[30s]) + vector(1), "service", "$1-api", "app", "(.*)"))"#;
+    let loki_result = loki_format_query_result(&http, &loki_base, query).await;
+    let crabka_result = crabka_format_query_result(querier.clone(), query).await;
+
+    assert!(crabka_result == loki_result);
+
     let query = r#"quantile_over_time(.75,{app="api"}|logfmt|unwrap cost[30s]) by(app)"#;
     let loki_result = loki_format_query_result(&http, &loki_base, query).await;
     let crabka_result = crabka_format_query_result(querier.clone(), query).await;
